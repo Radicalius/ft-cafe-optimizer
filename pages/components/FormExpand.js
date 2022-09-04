fe61dc556db8bb4489e31841ce19c61b6a450877
Zachary Cotton <mr.zacharycotton@gmail.com>
2022-09-04 14:14:01 -0700

    Form Expander

---
+++ pages/components/FormExpand.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import style from '../../styles/formexpand.module.css';
+
+export default function FormExpand({children}) {
+    const [show, setShown] = useState(false);
+
+    return (
+        <div className={style.form_expand}>
+            <div className={style.form_expand_header} onClick={() => setShown(!show)}>
+                <span className={style.form_expand_header_options}>
+                    <img className={style.gear_icon} src='https://upload.wikimedia.org/wikipedia/commons/9/96/Gear-icon-transparent-background.png' />
+                    Options
+                </span>
+                <span className={style.form_expand_header_toggle}>{show ? '▼' : '▲'}</span>
+            </div>
+            <div className={style.form_expand_contents} hidden={!show}>
+                {children}
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,42 @@
 import { useEffect, useState } from 'react';
 import ComboTable from './components/ComboTable';
-import Paginator from './components/Paginator';
+import FormExpand from './components/FormExpand';
 import { getMenu, getCombos } from './lib/data';
 
 export default function Home({ menu, initCombos, initMaxPage }) {
 
-  // const [combos, setCombos] = useState(initCombos);
-  // const [page, setPage] = useState(1);
-  // const [maxPage, setMaxPage] = useState(initMaxPage);
+  var [combos, setCombos] = useState(initCombos);
+  var [page, setPage] = useState(1);
+  var [maxPage, setMaxPage] = useState(initMaxPage);
 
-  // useEffect(() => {
-  //   fetch(`/api/combos?page=${page}&pagesize=5`)
-  //     .then(x => x.json())
-  //     .then(resp => {
-  //       setCombos(resp.combos);
-  //       setPage(resp.page);
-  //       setMaxPage(resp.maxPage);
-  //     });
-  // }, [page]);
+  function loadNextPage() {
+    setPage(page + 1);
+  }
+
+  useEffect(() => {
+    var blocked = false;
+    window.addEventListener('scroll', () => {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight && !blocked) {
+        if (page < maxPage) {
+          loadNextPage();
+          blocked = true;
+        }
+      } else {
+        blocked = false;
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(page);
+    fetch(`/api/combos?page=${page}&pagesize=5`)
+      .then(x => x.json())
+      .then(resp => {
+        setCombos(combos.concat(resp.combos));
+        setPage(resp.page);
+        setMaxPage(resp.maxPage);
+      });
+  }, [page]);
 
   // return (
   //   <center>
@@ -26,18 +45,21 @@ export default function Home({ menu, initCombos, initMaxPage }) {
   //     <Paginator page={page} maxPage={maxPage ? maxPage : initMaxPage} setPage={(x) => setPage(x)} />
   //   </center>
 
-    return (
-      <div id="page_container">
-        <ComboTable menu={menu} combos={initCombos} />
-      </div>
-    )
+  return (
+    <div id="page_container">
+      <h1 id="title">Franklin Templeton Cafe Optimizer</h1>
+      <p id="description">Scrapes the menu from the Franklin Templeton Bon Appetit cafe and finds combinations of items that approach $25.</p>
+      <FormExpand></FormExpand>
+      <ComboTable menu={menu} combos={combos} />
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
   const menu = await getMenu();
   var combos = (await getCombos());
-  const maxPage = Math.ceil(combos.length / 25);
-  combos = (await getCombos()).slice(0,25);
+  const maxPage = Math.ceil(combos.length / 5);
+  combos = (await getCombos()).slice(0,5);
 
   return {
     props: {
